dba3d5fb72b7f2684b39f413c332c9be7c6498c2
Divetrainer <ian@divelab.tech>
2025-11-13 23:51:01 -0800

    added onewire option to run waterproof options

---
+++ ds1820b.py
@@ -0,0 +1,21 @@
+## one wire code
+## https://docs.micropython.org/en/latest/esp8266/tutorial/onewire.html
+
+from machine import Pin, I2C
+from time import sleep
+import onewire, ds18x20
+
+ow_pin = Pin(12)
+
+#onewire object creation
+sensor = ds18x20.DS18X20(onewire.OneWire(ow_pin))
+
+while (True):
+    print('temperatures:', end=' ')
+    sensor.convert_temp()
+    sleep(.750)
+
+    print()
+
+    sleep(2)
+
