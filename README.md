090cb039f9efec5e70158eb12711bfaea3b7276d
Divetrainer <ian@divelab.tech>
2025-11-14 23:04:55 -0800

    added oled repository and updated ds1820b to be the primary. will move to main before deployment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# TempController
+# Temp_Controller
+
+MicroPython script to read temperature in home server 
+
+
+Goal is to try and isolate and create a functional script that can then be modified to be pushed to other applications
+
--- ds1820b.py
+++ ds1820b.py
@@ -1,18 +1,42 @@
-## one wire example
-## https://docs.micropython.org/en/latest/esp8266/tutorial/onewire.html
+## Divetrainer
 
-from machine import Pin, I2C
+
+from machine import Pin, I2C, SPI
 from time import sleep
 import onewire, ds18x20
+from ssd1306 import SSD1306_I2C
 
-ow_pin = Pin(12)
+#pin values
 led = Pin("LED", Pin.OUT)
+ow_pin = Pin(12)
+scl_p21 = Pin(21, Pin.IN)
+sda_p20 = Pin(20, Pin.IN)
+scl_p11 = Pin(11, Pin.IN)
+sda_p10 = Pin(10, Pin.IN)
+
+
+
+screen_i2c = I2C(1, scl=scl_p11, sda=sda_p10, freq=400000)
+
+
+oled = SSD1306_I2C(128, 32, screen_i2c, addr=0x3C)
+
+
+
+i2c = I2C(0, scl=scl_p21, sda=sda_p20, freq=100000)
+temp_address = 0x48
+
+oled.poweron()
+oled.contrast(0)
+
+
 #onewire object
 sensor = ds18x20.DS18X20(onewire.OneWire(ow_pin))
 
-roms = sensor.scan()
-rec_temp = 0
 
+roms = sensor.scan()
+sensor_recorded_temp = 0
+amb_temp = 0
 while (True):
     
     sensor.convert_temp()
@@ -22,11 +46,24 @@ while (True):
         led.toggle()
         temp_f = (sensor.read_temp(rom) * 1.8) + 32
         
-        if temp_f != rec_temp:
-            print('temperatures:', end=' ')
-            print("{:.1f}".format(temp_f), end=' ')
+        if temp_f != sensor_recorded_temp:
+            oled.fill(0)
+            oled.text('Sensor temp:', 0,0)
+            oled.text("{:}".format(temp_f),0,20)
+            oled.show()
             print()
-        
-        rec_temp = temp_f
-        led.toggle()
+        sensor_recorded_temp = temp_f
+    
+    temp_data_byte = i2c.readfrom(temp_address, 1, False)
+    temp_data_c = int.from_bytes(temp_data_byte, "big")
+    temp_data_f = (temp_data_c * 1.8) + 32
+    
+    if temp_data_f != amb_temp:
+        print("Ambient temperature:", "{:.1f}".format(temp_data_f), " F")
+        print()
+    
+    amb_temp = temp_data_f
 
+    led.toggle()
+
+oled.poweroff()
